4f90834d58dac36b446bb84b23d5de67ff505633
SergeyVolik <volik.sergey112@gmail.com>
2023-06-11 13:43:34 +0300

    :sparkles: logger updated

--- Runtime/Logger.cs
+++ Runtime/Logger.cs
@@ -1,6 +1,58 @@
+using System.Diagnostics;
+using UnityEngine;
 
-
-public class Logger
+namespace SV
 {
-    
+    public interface ILogger
+    {
+        public void Log(string str);
+        public void LogError(string str);
+        public void LogWarning(string str);
+    }
+
+    public class DefaultLogger : ILogger
+    {
+        public void Log(string str)
+        {
+            UnityEngine.Debug.Log(str);
+        }
+
+        public void LogError(string str)
+        {
+            UnityEngine.Debug.LogError(str);
+        }
+
+        public void LogWarning(string str)
+        {
+            UnityEngine.Debug.LogError(str);
+        }
+    }
+
+    public class CustomLogger
+    {
+
+        public static ILogger Instance = new DefaultLogger();
+        public const string DedSymbol = "EnableLogger";
+
+        [Conditional(DedSymbol)]
+        public static void Log(string str)
+        {
+            Instance.Log(str);
+        }
+
+        [Conditional(DedSymbol)]
+        public static void LogWarning(string str)
+        {
+            Instance.LogWarning(str);
+
+        }
+
+        [Conditional(DedSymbol)]
+        public static void LogError(string str)
+        {
+            Instance.LogError(str);
+        }
+
+       
+    }
 }
